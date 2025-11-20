25cdb51e9b4873a79a39d67c2150378ceba0e837
Gail Leanne Loking <gailloks123@gmail.com>
2025-11-20 21:32:15 +0800

    Add files via upload

---
+++ models.py
@@ -0,0 +1,21 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    category = db.Column(db.String(100), nullable=False)
+    amount = db.Column(db.Integer, default=1)
+    status = db.Column(db.String(20), default="available")
+    description = db.Column(db.String(200), nullable=True)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "category": self.category,
+            "amount": self.amount,
+            "status": self.status,
+            "description": self.description
+        }
